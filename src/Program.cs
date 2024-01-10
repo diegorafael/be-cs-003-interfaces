4fc308c1b638511f33ce3e575a077ebd8af4c744
Diego Souza <letscodeada-diegosouza@outlook.com>
2024-01-10 18:07:51 +0000

    add initial version

---
+++ src/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Tagarelo
+{
+    internal class Program
+    {
+        const string COMANDO_SAIR = "SAIR";
+
+        static void Main(string[] args)
+        {
+            var tagarelador = new Tagarelador();
+
+            Console.Clear();
+            TextoEmCor("------- INICIO -------", ConsoleColor.Gray);
+            Console.WriteLine($"Digite uma mensagem para o programa Tagarelo. Para finalizar digite '{COMANDO_SAIR}'");
+
+            while (true)
+            {
+                Console.WriteLine();
+
+                var mensagem = Console.ReadLine();
+
+                if (COMANDO_SAIR.ToUpper().Trim() != mensagem.ToUpper().Trim())
+                {
+                    TextoEmCor(tagarelador.Tagarelar(), ConsoleColor.Magenta);
+                }
+                else 
+                    break;
+            }
+
+            Console.WriteLine();
+            TextoEmCor("------------ FIM ------------", ConsoleColor.Gray);
+        }
+
+        private static void TextoEmCor(string conteudo, ConsoleColor cor)
+        {
+            ConsoleColor anterior = Console.ForegroundColor;
+            Console.ForegroundColor = cor;
+            Console.WriteLine(conteudo);
+            Console.ForegroundColor = anterior;
+        }
+    }
+}
---
+++ src/Tagarelador.cs
@@ -0,0 +1,19 @@
+﻿namespace Tagarelo
+{
+    internal class Tagarelador
+    {
+        string[] respostas = [
+            "Blá, blá, blá.",
+            "Lero, lero, lero",
+            "Ti, ti, ti",
+            "Ohmn...",
+            "Ahn?!",
+        ];
+
+        public string Tagarelar()
+        {
+            var aleatorio = new Random().Next(respostas.Length);
+            return respostas[aleatorio];
+        }
+    }
+}
